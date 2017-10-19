1bb88af2d8db6870df838beb9665439bcb41472a
Eduardo Silva <zedudu@gmail.com>
2017-10-19 17:50:00 -0200

    Add semicolon just to avoid warnings.

--- server/index.js
+++ server/index.js
@@ -5,43 +5,43 @@ io 			= require('socket.io')(server),
 path 		= require('path'),
 bodyParser 	= require('body-parser'),
 publicPath 	= '/../public/',
-liveCart
+liveCart;
 
-console.log('Hunts Point POS')
-console.log('Server started')
+console.log('Hunts Point POS');
+console.log('Server started');
 
-app.use(express.static(path.resolve(__dirname + publicPath)))
-app.use(express.static(path.resolve(__dirname + '/../bower_components')))
+app.use(express.static(path.resolve(__dirname + publicPath)));
+app.use(express.static(path.resolve(__dirname + '/../bower_components')));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.resolve(__dirname, publicPath, 'index.html'))
-})
+	res.sendFile(path.resolve(__dirname, publicPath, 'index.html'));
+});
 
-app.use('/api', require('./api'))
+app.use('/api', require('./api'));
 
 // Websocket logic for Live Cart
 io.on('connection', function (socket) {
 
 	socket.on('cart-transaction-complete', function () {
-		socket.broadcast.emit('update-live-cart-display', {})
-	})
+		socket.broadcast.emit('update-live-cart-display', {});
+	});
 
 	// upon page load, give user current cart
 	socket.on('live-cart-page-loaded', function () {
-		socket.emit('update-live-cart-display', liveCart)
-	})
+		socket.emit('update-live-cart-display', liveCart);
+	});
 
 	// upon connecting, make client update live cart
-	socket.emit('update-live-cart-display', liveCart)
+	socket.emit('update-live-cart-display', liveCart);
 
 	// when the cart data is updated by the POS
 	socket.on('update-live-cart', function (cartData) {
-		
+
 		// keep track of it
-		liveCart = cartData
-		
+		liveCart = cartData;
+
 		// broadcast updated live cart to all websocket clients
-		socket.broadcast.emit('update-live-cart-display', liveCart)
-	})
+		socket.broadcast.emit('update-live-cart-display', liveCart);
+	});
 // test comment insert pranavi
-})
+});
